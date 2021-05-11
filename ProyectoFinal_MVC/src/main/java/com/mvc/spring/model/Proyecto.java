d520cb1eeff448e79abd64b21bb9795ce6481ab8
yelder <yelder302@gmail.com>
2021-05-11 14:26:38 +0200

    Se ha realizado el controller de ProyectosMVC

--- ProyectoFinal_MVC/src/main/java/com/mvc/spring/model/Proyecto.java
+++ ProyectoFinal_MVC/src/main/java/com/mvc/spring/model/Proyecto.java
@@ -1,17 +1,9 @@
 package com.mvc.spring.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
-@Entity
-@Table(name = "proyectos")
+
 public class Proyecto {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int idproyecto;
 	private String proyecto;
 	private String fechafin;
