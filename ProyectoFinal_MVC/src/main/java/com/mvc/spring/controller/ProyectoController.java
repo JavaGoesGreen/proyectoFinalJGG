535587275e7b67602531937dbbb0e52aadf683c8
Antoni Blanche <antoni.bx@gmail.com>
2021-05-13 10:27:44 +0200

    HTML y demas

--- ProyectoFinal_MVC/src/main/java/com/mvc/spring/controller/ProyectoController.java
+++ ProyectoFinal_MVC/src/main/java/com/mvc/spring/controller/ProyectoController.java
@@ -10,52 +10,62 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.mvc.spring.model.Proyecto;
 import com.mvc.spring.service.ProyectosServiceImpl;
+/**
+ * <p><b> Nombre </b> ProyectoController</p>
+ * 
+ * <p><strong>Descripcion </strong> controlador del proyecto MVC que recibe json</p>
+ * 
+ * @author	Toni Blanche
+ * 
+ * @version	v1
+ * 
+ * @since	13/05/2021
+ */
 
 @Controller
-/*@RequestMapping("/admin/proyectos")*/
+/* @RequestMapping("/admin/proyectos") */
 public class ProyectoController {
 
-	@Autowired 
+	@Autowired
 	ProyectosServiceImpl service;
-	
+
 	@GetMapping("/proyectos")
 	public String listaProyectos(Model m) {
 		m.addAttribute("proyectoslista", service.getProyectos());
 		return "proyectos";
 	}
-	
+
 	@GetMapping("/proyectos/admin/list")
 	public String listaProyectosAdmin(Model m) {
 		m.addAttribute("proyectoslista", service.getProyectos());
 		return "admin/proyectosadmin";
 	}
-	
-	//nuevo usuario
+
+	// nuevo usuario
 	@GetMapping("proyectos/admin/add")
 	public String newUser(Proyecto proyecto) {
 		return "/admin/addProyecto";
 	}
-	
+
 	@PostMapping("proyectos/admin/post")
-	public ModelAndView addProyecto(Proyecto proyecto) {	
+	public ModelAndView addProyecto(Proyecto proyecto) {
 		service.addProyectos(proyecto);
 		return new ModelAndView("redirect:/");
 	}
-	
-	
-	//referencias a otras paginas
+
+	// referencias a otras paginas
 	@GetMapping("/contacto")
 	public String contacto() {
 
 		return "contacto";
 	}
-	
+
 	@GetMapping("/equipo")
 	public String equipo() {
 
 		return "equipo";
 	}
-	
+
 	@GetMapping("/index")
 	public String index() {
 
